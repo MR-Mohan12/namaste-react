90bfdaf571cc1541853cdf0b6e7c383110fc1192
MR-MOHAN12 <maniofficial234@gmail.com>
2024-01-10 09:36:02 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,25 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },[
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "i am h1 tag"),
+    React.createElement("h2", {}, "i am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "i am h1 tag"),
+    React.createElement("h2", {}, "i am h2 tag"),
+  ]),
+]
+);
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
+
+// const heading =  React.createElement("h1",{id: "heading"},"Hello World from React!");
+// console.log(heading);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// // console.log(root);
+// root.render(heading);
